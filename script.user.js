28c9a9dba50b53f18f0255330061545465392754
Kamil Neumann <kamneu04@gmail.com>
2024-12-09 16:06:13 +0100

    first commit

---
+++ script.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         forsenE
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-09
+// @description  GACHI THE WEEBS AWAY
+// @author       NevMan
+// @match        https://www.reddit.com/r/forsen/comments/*
+// @updateURL    
+// @downloadURL  
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        const node = document.createElement("button");
+        node.innerHTML = "Downvote all retards";
+        node.addEventListener('click', asd);
+        document.getElementById("right-sidebar-container").appendChild(node);
+        document.getElementsByTagName("shreddit-post")[0].shadowRoot.appendChild(node);
+        console.log("done");
+        asd();
+        //
+    }, false);
+})();
+
+function asd() {
+    let comments = document.getElementsByTagName("shreddit-comment");
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].children[2].innerHTML.toString().includes("https://reddit-econ-prod-assets-permanent.s3.amazonaws.com/asset-manager/t5_33td5/neRMhyZ4QN.png")) {
+            let button = comments[i].getElementsByTagName("shreddit-comment-action-row")[0].shadowRoot.firstElementChild.getElementsByTagName("button")[2];
+            if (button.ariaPressed == 'false') {
+                console.log("false")
+                button.click();
+            }
+            else {
+                console.log("true");
+            }
+        }
+    }
+}
