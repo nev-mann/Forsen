beb421d555594349a792b6c0e435a668f1de15d2
Kamil Neumann <kamneu04@gmail.com>
2024-12-09 17:33:28 +0100

    3

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         forsenE
+// @name         Downvote weebs
 // @namespace    http://tampermonkey.net/
-// @version      2
+// @version      3
 // @description  GACHI THE WEEBS AWAY
 // @author       NevMan
 // @match        https://www.reddit.com/r/forsen/comments/*
@@ -9,33 +9,28 @@
 // @downloadURL  https://github.com/nev-mann/Reddit-forsen-script/raw/refs/heads/main/script.user.js
 // ==/UserScript==
 
+const emoteToDownvote = "https://reddit-econ-prod-assets-permanent.s3.amazonaws.com/asset-manager/t5_33td5/neRMhyZ4QN.png";
+
+function main() {
+    let comments = document.getElementsByTagName("shreddit-comment");
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].children[2].innerHTML.toString().includes(emoteToDownvote)) {
+            let button = comments[i].getElementsByTagName("shreddit-comment-action-row")[0].shadowRoot.firstElementChild.getElementsByTagName("button")[2];
+            if (button.ariaPressed == 'false') {
+                button.click();
+            }
+        }
+    }
+}
 
 (function () {
     'use strict';
     window.addEventListener('load', function () {
         const node = document.createElement("button");
-        node.innerHTML = "Downvote all retards";
-        node.addEventListener('click', asd);
-        document.getElementById("right-sidebar-container").appendChild(node);
+        node.innerHTML = "Downvote all weebs";
+        node.addEventListener('click', main);
         document.getElementsByTagName("shreddit-post")[0].shadowRoot.appendChild(node);
-        console.log("done");
-        asd();
+        main();
         //
     }, false);
 })();
-
-function asd() {
-    let comments = document.getElementsByTagName("shreddit-comment");
-    for (let i = 0; i < comments.length; i++) {
-        if (comments[i].children[2].innerHTML.toString().includes("https://reddit-econ-prod-assets-permanent.s3.amazonaws.com/asset-manager/t5_33td5/neRMhyZ4QN.png")) {
-            let button = comments[i].getElementsByTagName("shreddit-comment-action-row")[0].shadowRoot.firstElementChild.getElementsByTagName("button")[2];
-            if (button.ariaPressed == 'false') {
-                console.log("false")
-                button.click();
-            }
-            else {
-                console.log("true");
-            }
-        }
-    }
-}
