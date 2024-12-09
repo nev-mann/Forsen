a3d8755e91b5001606e1be43d00e2f5f903dbaa1
Kamil Neumann <kamneu04@gmail.com>
2024-12-09 17:58:23 +0100

    finale version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Downvote weeb emotes
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      3
 // @description  GACHI THE WEEBS AWAY
 // @author       NevMan
 // @match        https://www.reddit.com/*
@@ -11,8 +11,22 @@
 const emoteToDownvote = "https://reddit-econ-prod-assets-permanent.s3.amazonaws.com/asset-manager/t5_33td5/neRMhyZ4QN.png";
 const guyToDownvote = "Vocaloid-Guy";
 
+let oldPage = "";
+
 function main() {
     if (!window.location.toString().startsWith("https://www.reddit.com/r/forsen/comments/")) return;
+    if (oldPage == window.location.toString()) return;
+    oldPage = window.location.toString();
+
+    let node = document.createElement("button");
+    node.innerHTML = "Downvote all weebs";
+    node.addEventListener('click', downvote);
+    document.getElementsByTagName("shreddit-post")[0].shadowRoot.appendChild(node);
+
+    downvote();
+}
+
+function downvote() {
     let comments = document.getElementsByTagName("shreddit-comment");
     for (let i = 0; i < comments.length; i++) {
         if (comments[i].children[2].innerHTML.toString().includes(emoteToDownvote) || comments[i].__author == guyToDownvote) {
