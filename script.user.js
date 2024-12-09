5e98712a998d3b450e4b8db76c7b126bef4577f9
Kamil Neumann <kamneu04@gmail.com>
2024-12-09 17:45:30 +0100

    4

--- script.user.js
+++ script.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
-// @name         Downvote weebs
+// @name         Downvote weeb emotes
 // @namespace    http://tampermonkey.net/
-// @version      3
+// @version      4
 // @description  GACHI THE WEEBS AWAY
 // @author       NevMan
-// @match        https://www.reddit.com/r/forsen/comments/*
+// @match        https://www.reddit.com/*
 // @updateURL    https://github.com/nev-mann/Reddit-forsen-script/raw/refs/heads/main/script.user.js
 // @downloadURL  https://github.com/nev-mann/Reddit-forsen-script/raw/refs/heads/main/script.user.js
 // ==/UserScript==
-
 const emoteToDownvote = "https://reddit-econ-prod-assets-permanent.s3.amazonaws.com/asset-manager/t5_33td5/neRMhyZ4QN.png";
+const guyToDownvote = "Vocaloid-Guy";
 
 function main() {
+    if (!window.location.toString().startsWith("https://www.reddit.com/r/forsen/comments/")) return;
     let comments = document.getElementsByTagName("shreddit-comment");
     for (let i = 0; i < comments.length; i++) {
-        if (comments[i].children[2].innerHTML.toString().includes(emoteToDownvote)) {
+        if (comments[i].children[2].innerHTML.toString().includes(emoteToDownvote) || comments[i].__author == guyToDownvote) {
             let button = comments[i].getElementsByTagName("shreddit-comment-action-row")[0].shadowRoot.firstElementChild.getElementsByTagName("button")[2];
             if (button.ariaPressed == 'false') {
                 button.click();
@@ -25,12 +26,5 @@ function main() {
 
 (function () {
     'use strict';
-    window.addEventListener('load', function () {
-        const node = document.createElement("button");
-        node.innerHTML = "Downvote all weebs";
-        node.addEventListener('click', main);
-        document.getElementsByTagName("shreddit-post")[0].shadowRoot.appendChild(node);
-        main();
-        //
-    }, false);
+    setInterval(main, 2000);
 })();
